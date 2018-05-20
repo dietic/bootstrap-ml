561cbe50fea20e41eb3af029adb5ce656c09abf7
dietic <dierios93@gmail.com>
2018-05-20 17:56:55 -0500

    Footer

--- incs/footer.php
+++ incs/footer.php
@@ -1,3 +1,8 @@
+<footer class="footer bg-light">
+	<div class="container-fluid">
+		<p style="margin: 0; text-align: center;">&copy; Copyright - 2018</p>
+	</div>
+</footer>
 </div>
 	<script src="js/jquery.min.js" type="text/javascript"></script>
 	<script src="js/bootstrap.min.js" type="text/javascript"></script>
--- index.php
+++ index.php
@@ -1,50 +1,5 @@
 <?php include 'incs/header.php'; ?>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	
-	  <a class="navbar-brand" href="#">Navbar</a>
-	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-	    <span class="navbar-toggler-icon"></span>
-	  </button>
-	  
-	  	<div class="collapse navbar-collapse " id="navbarSupportedContent">
-		  	<div class="col">
-			    <ul class="navbar-nav">
-			      <li class="nav-item active">
-			        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-			      </li>
-			      <li class="nav-item dropdown">
-			        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-			          Dropdown
-			        </a>
-			        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-			          <a class="dropdown-item" href="#">Action</a>
-			          <a class="dropdown-item" href="#">Another action</a>
-			          <div class="dropdown-divider"></div>
-			          <a class="dropdown-item" href="#">Something else here</a>
-			        </div>
-			      </li>
-			      
-			    </ul>
-		    </div>
-		    <div class="col text-center">
-			  	<form class="form-inline my-2 my-lg-0">
-			      <input class="navbar-search form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-			      <i class="fas fa-search"></i>
-			    </form>
-		    </div>
-		    <div class="dropdown-divider"></div>
-		    <div class="col">
-		    	<ul class="navbar-nav justify-content-end" >
-			      <li class="nav-item ls-items">
-			        <a class="nav-link" href="#">Registro</a>
-			      </li>
-			      <li class="nav-item ls-items">
-			        <a class="nav-link" href="#">Ingreso</a>
-			      </li>
-			  </ul>
-		    </div>	
-	    </div>	
-</nav>
+<?php include 'incs/navbar.php'; ?>
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
   	<ol class="carousel-indicators">
 	    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -64,7 +19,7 @@
   	</div>
 </div>
 <div class="row">
-	<?php for($i = 0; $i < 10; $i++) { ?>
+	<?php for($i = 0; $i < 5; $i++) { ?>
 	<div class="card col-6 col-lg-4">
 		<img class="card-img-top" src="images/ipad-placeholder.jpg" alt="Card image cap">
 		<div class="card-body">
@@ -75,9 +30,6 @@
 	  	</ul>
 	</div>
 	<?php } ?>
-	
-
-
 </div>
 
 
