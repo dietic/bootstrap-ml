8572d8eaef18264ffda57ecccfb6a2ea230a320e
dietic <dierios93@gmail.com>
2018-05-20 16:11:29 -0500

    First push

---
+++ index.php
@@ -0,0 +1,126 @@
+<?php include 'incs/header.php'; ?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	
+	  <a class="navbar-brand" href="#">Navbar</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  
+	  	<div class="collapse navbar-collapse " id="navbarSupportedContent">
+		  	<div class="col">
+			    <ul class="navbar-nav">
+			      <li class="nav-item active">
+			        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			      </li>
+			      <li class="nav-item dropdown">
+			        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			          Dropdown
+			        </a>
+			        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+			          <a class="dropdown-item" href="#">Action</a>
+			          <a class="dropdown-item" href="#">Another action</a>
+			          <div class="dropdown-divider"></div>
+			          <a class="dropdown-item" href="#">Something else here</a>
+			        </div>
+			      </li>
+			      
+			    </ul>
+		    </div>
+		    <div class="col text-center">
+			  	<form class="form-inline my-2 my-lg-0">
+			      <input class="navbar-search form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+			      <i class="fas fa-search"></i>
+			    </form>
+		    </div>
+		    <div class="dropdown-divider"></div>
+		    <div class="col">
+		    	<ul class="navbar-nav justify-content-end" >
+			      <li class="nav-item ls-items">
+			        <a class="nav-link" href="#">Registro</a>
+			      </li>
+			      <li class="nav-item ls-items">
+			        <a class="nav-link" href="#">Ingreso</a>
+			      </li>
+			  </ul>
+		    </div>	
+	    </div>	
+</nav>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  	<ol class="carousel-indicators">
+	    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  	</ol>
+  	<div class="carousel-inner">
+    	<div class="carousel-item active">
+      		<img class="d-block w-100" src="images/carrousel-placeholder1.jpg" alt="First slide">
+    	</div>
+	    <div class="carousel-item">
+	      	<img class="d-block w-100" src="images/carrousel-placeholder2.jpg" alt="Second slide">
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100" src="images/carrousel-placeholder3.jpg" alt="Third slide">
+	    </div>
+  	</div>
+</div>
+
+<div class="card-deck">
+	<div class="card">
+		<img class="card-img-top" src="images/ipad-placeholder.jpg" alt="Card image cap">
+		<div class="card-body">
+		  <h5 class="card-title">Card title</h5>
+		  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+		</div>
+		<ul class="list-group list-group-flush">
+		    <li class="list-group-item">Cras justo odio</li>
+	  	</ul>
+	</div>
+	<div class="card">
+		<img class="card-img-top" src="images/ipad-placeholder.jpg" alt="Card image cap">
+		<div class="card-body">
+		  <h5 class="card-title">Card title</h5>
+		  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+		</div>
+		<ul class="list-group list-group-flush">
+		    <li class="list-group-item">Cras justo odio</li>
+	  	</ul>
+	</div>
+	<div class="card">
+		<img class="card-img-top" src="images/ipad-placeholder.jpg" alt="Card image cap">
+		<div class="card-body">
+		  <h5 class="card-title">Card title</h5>
+		  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+		</div>
+		<ul class="list-group list-group-flush">
+		    <li class="list-group-item">Cras justo odio</li>
+	  	</ul>
+	</div> 
+</div>
+
+<div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="images/ipad-placeholder.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="images/ipad-placeholder.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="images/ipad-placeholder.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+  </div>
+</div>
+
+<?php include 'incs/footer.php'; ?>
+		
+	
