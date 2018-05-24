476999e7ea9d02c496588f74d89e680c2f1c00ea
Rios <diego.rios@cadreon.com>
2018-05-24 13:43:14 -0500

    PHP for ADDED

--- index.php
+++ index.php
@@ -39,6 +39,7 @@
 	<?php /*}*/ ?>
 </div>-->
 
+<?php for($i = 0; $i < 3; $i++) { ?>
 <div class="row secciones">
     <div class="left-col col-12 col-sm-6 col-lg-6">
         <p></p>
@@ -50,6 +51,7 @@
         </div>
     </div>
 </div>
+<?php } ?>
 <div class="row test" style="background-color:red; height:50px;"></div>
 
 <?php include 'incs/footer.php'; ?>
