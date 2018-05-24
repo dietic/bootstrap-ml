fab2b92da2912fa29233ffcb8ef6f5d127dd2f8e
Rios <diego.rios@cadreon.com>
2018-05-24 13:59:14 -0500

    Add CARROUSEL to incs and added space between it and items

---
+++ incs/carrousel.php
@@ -0,0 +1,18 @@
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  	<ol class="carousel-indicators">
+	    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  	</ol>
+  	<div class="carousel-inner">
+    	<div class="carousel-item active">
+      		<img class="d-block w-100" src="images/carrousel-placeholder1.jpg" alt="First slide">
+    	</div>
+	    <div class="carousel-item">
+	      	<img class="d-block w-100" src="images/carrousel-placeholder2.jpg" alt="Second slide">
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100" src="images/carrousel-placeholder3.jpg" alt="Third slide">
+	    </div>
+  	</div>
+</div>
--- index.php
+++ index.php
@@ -1,57 +1,21 @@
 <?php include 'incs/header.php'; ?>
 <?php include 'incs/navbar.php'; ?>
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  	<ol class="carousel-indicators">
-	    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-	    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-	    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  	</ol>
-  	<div class="carousel-inner">
-    	<div class="carousel-item active">
-      		<img class="d-block w-100" src="images/carrousel-placeholder1.jpg" alt="First slide">
-    	</div>
-	    <div class="carousel-item">
-	      	<img class="d-block w-100" src="images/carrousel-placeholder2.jpg" alt="Second slide">
-	    </div>
-	    <div class="carousel-item">
-	      <img class="d-block w-100" src="images/carrousel-placeholder3.jpg" alt="Third slide">
-	    </div>
-  	</div>
-</div>
-<!--<div class="row">as
-	<?php /*for($i = 0; $i < 5; $i++) { */?>
-	<div class="card col-6 col-lg-4">
-		<img class="card-img-top" src="images/ipad-placeholder.jpg" alt="Card image cap">
-		<div class="card-body">
-		  <h5 class="card-title text-center">iPad air</h5>
-		  <div class="text-center">
-			  <i class="far fa-star"></i>
-			  <i class="far fa-star"></i>
-			  <i class="far fa-star"></i>
-			  <i class="far fa-star"></i>
-			  <i class="far fa-star"></i>
-		  </div>
-		</div>
-		<ul class="list-group list-group-flush">
-		    <li class="list-group-item text-center"><b>$ 35</b></li>
-	  	</ul>
-	</div>
-	<?php /*}*/ ?>
-</div>-->
-
-<?php for($i = 0; $i < 3; $i++) { ?>
-<div class="row secciones">
-    <div class="left-col col-12 col-sm-6 col-lg-6">
-        <p></p>
-    </div>
-    <div class="right-col col-12 col-sm-6 col-lg-6">
-        <div class="inside" style="background-color: #FFEBEA;">
+<?php include 'incs/carrousel.php'; ?>
+<div class="secciones-wrapper">
+    <?php for($i = 0; $i < 3; $i++) { ?>
+    <div class="row secciones">
+        <div class="left-col col-12 col-sm-6 col-lg-6">
+            <p></p>
         </div>
-        <div class="inside" style="background-color: #FFEBEA; background-color: black;">
+        <div class="right-col col-12 col-sm-6 col-lg-6">
+            <div class="inside" style="background-color: #FFEBEA;">
+            </div>
+            <div class="inside" style="background-color: #FFEBEA; background-color: black;">
+            </div>
         </div>
     </div>
+    <?php } ?>
 </div>
-<?php } ?>
 <div class="row test" style="background-color:red; height:50px;"></div>
 
 <?php include 'incs/footer.php'; ?>
